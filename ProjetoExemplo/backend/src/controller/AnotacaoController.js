7629abc8b54212b74b3772a75b2216da51255d88
aluno <Aluno@PR01-COMP2-14.CSED.LAB>
2023-04-13 10:19:28 -0300

    pt.1

--- ProjetoExemplo/backend/src/controller/AnotacaoController.js
+++ ProjetoExemplo/backend/src/controller/AnotacaoController.js
@@ -6,6 +6,9 @@ module.exports = {
     },
     async create(req, res){
         const {titulo, notas, prioridade} = req.body;
+        if(!titulo || !notas){
+            return res.status(400).json({error:"É necessario preencher um titulo e uma anotacao."});
+        }
         const AnotacaoCriada = await Anotacoes.create({
             titulo,
             notas,
@@ -13,5 +16,13 @@ module.exports = {
         });
         return res.json(AnotacaoCriada);
             
+    },
+
+    async delete (req,res){
+        const { id } = req.params;
+        const anotacaoDeletadas = await Anotacoes.findOneAndDelete({ _id: id});
+        if(anotacaoDeletadas){
+            return res.json(anotacaoDeletadas);
+        }
     }
 }
--- ProjetoExemplo/backend/src/rotas.js
+++ ProjetoExemplo/backend/src/rotas.js
@@ -4,8 +4,9 @@ const AnotacaoController = require("./controller/AnotacaoController");
 
 // Seção para definição das rotas de anotações
 // rotas.get("/anotacoes", AnotacaoController.read);
-
+rotas.get("/anotacoes", AnotacaoController.read);
 rotas.post("/anotacoes", AnotacaoController.create);
+rotas.delete("/anotacoes/_id", AnotacaoController.delete);
 
 
 module.exports = rotas;
