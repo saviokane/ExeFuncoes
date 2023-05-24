894a035bd36677293bbfe1a5c746d5ef6b27ea77
Savio Kane <savio.kanesk@gmail.com>
2023-05-24 20:56:31 -0300

    @temp

---
+++ ClienteVendas/src/Controller/FormularioController.js
@@ -0,0 +1,93 @@
+const Cliente = require("../model/VendaModel");
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const fs = require('fs');
+
+// Metodo usando handlebar, porem ao return res.render('formulario');
+// não estava sendo encontrado o arquivo 'formulario' 
+const handlebars = require('express-handlebars');
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// retornando o body-parser
+
+const bodyParser = require('body-parser');
+const { off } = require('process');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+module.exports = {
+    async cadastro (req,res){
+       const filePath = path.join(__dirname,'../views/formulario.html')
+
+       fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error('Erro ao ler o arquivo:', err);
+          return res.status(500).send('Ocorreu um erro ao processar a solicitação.');
+        }
+    
+        res.set('Content-Type', 'text/html');
+        res.send(data);
+      });
+    },
+
+    async receber (req,res){
+        // const meuCheckbox = req.body.fidelidade || false;
+
+        const {nome,cpf,email,telefone,fidelidade} = req.body;
+        res.send("Nome: "+req.body.nome+
+                 "<br>CPF: "+req.body.cpf+
+                 "<br>Email: "+req.body.email+
+                 "<br>Telefone: "+req.body.telefone+
+                 "<br>Fidelidade: "+req.body.fidelidade);
+    },
+
+    async sendtodb (req,res){
+        const {nome,cpf,email,telefone,fidelidade} = req.body;
+        
+        const clienteCreate = await Cliente.create ({
+            nome,
+            cpf,
+            email,
+            telefone,
+            fidelidade
+        });
+
+        clienteCreate.nome = nome;
+        clienteCreate.cpf = cpf;
+        clienteCreate.email = email;
+        clienteCreate.telefone = telefone;
+        clienteCreate.fidelidade = fidelidade;
+
+            console.log("Cliente novo salvo pow: ",clienteCreate.nome);
+            new Cliente(clienteCreate).save().then(() => {
+                console.log("Cliente novo salvo.");
+            })
+
+    }
+}
+// metodo usando html in-line JS
+/*
+    async cadastro (req,res){
+        const html = 
+        `<html>
+        <body>
+            <form action="/add" method="post">
+                <p>Nome: </p>
+                <input type="text" name="nome">
+                <p>CPF: </p>
+                <input type="text" name="" id="cpf">
+                <p>Email: </p>
+                <input type="text" name="email">
+                <p>Fidelidade: </p>
+                <input type="checkbox" id="fidelizado" name="fidelidade"><br>
+                <input type="submit" name="Enviar">
+            </form>
+        </body>
+        </html>`;
+    return res.send(html);   
+},*/
--- ClienteVendas/src/index.js
+++ ClienteVendas/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const app = express();
 const rotas = require("./rotas");
+const app = express();
+const handlebars = require('express-handlebars');
+
+// configurando body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(rotas);
